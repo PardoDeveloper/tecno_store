d943f170e65943f05b4abeb9dd66dedeb6c82c8e
Maximiliano <max.rodriguezu@duocuc.cl>
2025-02-22 09:38:34 -0300

    first_commit

---
+++ tecno_store/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for tecno_store project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('tecno_store.apps.home.urls')),
+    path('payments/', include('tecno_store.apps.payments.urls')),
+    path('products/', include('tecno_store.apps.products.urls')),
+    path('users/', include('tecno_store.apps.users.urls')),
+]
